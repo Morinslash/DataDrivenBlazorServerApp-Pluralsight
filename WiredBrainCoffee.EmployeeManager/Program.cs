698ebeddd530363cb236d6e0616bfd5f9870b4ab
Sebastian Kulinski <sebastian.elf.kulinski@gmail.com>
2022-12-26 19:03:06 +0100

    finishing training

--- WiredBrainCoffee.EmployeeManager/Program.cs
+++ WiredBrainCoffee.EmployeeManager/Program.cs
@@ -1,10 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using WiredBrainCoffee.EmployeeManager.Data;
+using WiredBrainCoffee.EmployeeManager.Shared;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContextFactory<EmployeeManagerDbContext>(
+    opt => opt.UseSqlServer(
+        builder.Configuration.GetConnectionString("EmployeeManagerDb")));
+builder.Services.AddScoped<StateContainer>();
+
 
 var app = builder.Build();
+
+//development only
+await EnsureDatabaseIsMigrated(app.Services);
+
+async Task EnsureDatabaseIsMigrated(IServiceProvider serviceProvider)
+{
+    using var scope = serviceProvider.CreateScope();
+    using var context = scope.ServiceProvider.GetService<EmployeeManagerDbContext>();
+    if (context is not  null)
+    {
+         await context.Database.MigrateAsync();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
